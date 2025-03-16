140d38da61a318cf59e84e74438b1228e62e571f
Albert Ferguson <albertferguson118@gmail.com>
2025-03-16 17:30:51 +1100

    unplugin autoimport types

--- mapping-test/vite.config.ts
+++ mapping-test/vite.config.ts
@@ -3,10 +3,25 @@ import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import tsconfig from './tsconfig.app.json';
 import path from 'path';
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    AutoImport({
+      include: [
+        /\.[tj]s?$/, // .ts / .js
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\/.md$/, // .md
+      ],
+      dts: './src/types/auto-import.d.ts',
+      imports: ['vue', 'vitest', 'vue-router'],
+      vueTemplate: true,
+    }),
+    vueDevTools(),
+  ],
   resolve: {
     alias: Object.fromEntries(
       Object.entries(tsconfig.compilerOptions.paths).map(([key, value]) => [

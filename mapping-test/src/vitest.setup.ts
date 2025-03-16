c8a9cdd046020c4529601c3ae553d4bcc409e27c
Albert Ferguson <albertferguson118@gmail.com>
2025-03-16 16:53:04 +1100

    format all src/*

--- mapping-test/src/vitest.setup.ts
+++ mapping-test/src/vitest.setup.ts
@@ -1,12 +1,12 @@
-import 'vitest-canvas-mock'
+import 'vitest-canvas-mock';
 
 /**
  * TL;DR WERROR for tests (block tests that throw warnings)
  */
 function throwOnWarnings() {
   console.warn = (message: string) => {
-    throw new Error(`A warning occurred during test execution:\n${message}`)
-  }
+    throw new Error(`A warning occurred during test execution:\n${message}`);
+  };
 }
 
-throwOnWarnings()
+throwOnWarnings();

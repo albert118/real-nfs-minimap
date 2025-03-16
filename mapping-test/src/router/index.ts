46a4f55a6e7640d65d3182828f06ea4d5c8b0ad9
Albert Ferguson <albertferguson118@gmail.com>
2025-03-16 17:06:02 +1100

    vue router setup + config + comments

--- mapping-test/src/router/index.ts
+++ mapping-test/src/router/index.ts
@@ -1,23 +1,41 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory, type Router } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import { RouteName } from './routes';
 
-const router = createRouter({
+////////////////////////////////////////////////////////////////////////////////
+// Lazy Imports
+//    aka. "route level code-splitting"
+//    this generates a separate chunk (About.[hash].js) for this route
+//    which is lazy-loaded when the route is visited.
+////////////////////////////////////////////////////////////////////////////////
+
+const About = () => import('../views/AboutView.vue');
+
+export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: RouteName.Home,
       component: HomeView,
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+
+      component: About,
     },
   ],
 });
 
-export default router;
+////////////////////////////////////////////////////////////////////////////////
+// Extension Methods
+//    Implement any custom extension methods here. Don't forget to add them
+//    to the interface definition within ./vue-router.d.ts
+////////////////////////////////////////////////////////////////////////////////
+
+router.isCurrentRoute = isCurrentRoute;
+
+function isCurrentRoute(this: Router, routeName: RouteName) {
+  return this.currentRoute.value.name === routeName;
+}

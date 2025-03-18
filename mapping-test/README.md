1ed19dc7f576907bffbd3d72c6cf4f8da43f7fd3
Albert Ferguson <albertferguson118@gmail.com>
2025-03-19 00:02:11 +1100

    assets registered and PWA manifest configured + msic.

--- mapping-test/README.md
+++ mapping-test/README.md
@@ -7,11 +7,13 @@ TODO
 First add an icon to the `./public` directory. This should be,
 
 - 512x512px min.
-- an SVG or png
+- an SVG (for scaling and masking)
 
 ```sh
-npx pwa-asset-generator --preset minimal -i ./index.html ./public/icon.jpg ./public
+npx pwa-asset-generator ./public/icon.jpg ./public -i ./index.html --icon-only --favicon
 ```
 
 This command will print out the PWA asset config needed for the Vite PWA plugin config (ie. the manifest). Copy it into the PWA plugin config within
 `vite.config.ts`
+
+> Note that this will modify the `index.html` with the appropriate meta.
--- mapping-test/vite.config.ts
+++ mapping-test/vite.config.ts
@@ -3,7 +3,9 @@ import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import tsconfig from './tsconfig.app.json';
 import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 import AutoImport from 'unplugin-auto-import/vite';
+import packageDefintion from './package.json';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -21,6 +23,73 @@ export default defineConfig({
       vueTemplate: true,
     }),
     vueDevTools(),
+    // https://vite-pwa-org.netlify.app/
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-icon-180.png', 'maskable-icon-512.png', 'maskable-icon-192.png', 'favicon-196.png'],
+      manifest: {
+        name: packageDefintion.name,
+        short_name: packageDefintion.shortName,
+        description: packageDefintion.description,
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'public/manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'public/manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: 'public/manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'public/manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+      workbox: {
+        // TODO: implement offline caching with this configuration
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === 'style' || request.destination === 'script' || request.destination === 'worker',
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'static-resources',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+              },
+            },
+          },
+          {
+            urlPattern: ({ request }) => request.destination === 'image',
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'images',
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 60 * 24 * 60 * 30, // 30 days
+              },
+            },
+          },
+        ],
+      },
+      devOptions: {
+        enabled: isDevelopment(),
+      },
+    }),
   ],
   resolve: {
     alias: Object.fromEntries(
@@ -31,3 +100,7 @@ export default defineConfig({
     ),
   },
 });
+
+function isDevelopment() {
+  return process.env.NODE_ENV === 'development';
+}

e11555f0d5894a80793d5412452dcd35656a432e
Albert Ferguson <albertferguson118@gmail.com>
2025-03-22 14:12:34 +1100

    added Vuetify

--- mapping-test/src/main.ts
+++ mapping-test/src/main.ts
@@ -1,15 +1,29 @@
 import '@assets/main.css';
+
+// Leaflet
 import 'leaflet/dist/leaflet.css';
 
-import { createApp } from 'vue';
+// Pinia
 import { createPinia } from 'pinia';
 
+// Vuetify
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+import { createApp } from 'vue';
 import App from './App.vue';
 import { router } from '@router';
 import GlobalErrorHandler from '@plugins/GlobalErrorHandler';
 
 const app = createApp(App);
 
-app.use(GlobalErrorHandler).use(createPinia()).use(router);
+app
+  .use(GlobalErrorHandler)
+  .use(createPinia())
+  .use(router)
+  .use(createVuetify({ components, directives, }),
+);
 
 app.mount('#app');

d483edfcdd7da734bcf1939d6d97999476662d7d
Albert Ferguson <albertferguson118@gmail.com>
2025-04-20 17:46:21 +1000

    first build and added remaining files + generated type files

---
+++ nfs-styled-minimap/src/main.ts
@@ -0,0 +1,22 @@
+import '@assets/main.css';
+
+// Leaflet
+import 'leaflet/dist/leaflet.css';
+
+// Pinia
+import { createPinia } from 'pinia';
+
+// Vuetify
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+
+import { createApp } from 'vue';
+import App from './App.vue';
+import { router } from '@router';
+import GlobalErrorHandler from '@plugins/GlobalErrorHandler';
+
+const app = createApp(App);
+
+app.use(GlobalErrorHandler).use(createPinia()).use(router).use(createVuetify());
+
+app.mount('#app');

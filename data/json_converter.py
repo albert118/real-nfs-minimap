8bc2c905edf71b0be8e2518883e4380c34aa59e1
Albert Ferguson <albertferguson118@gmail.com>
2025-04-21 16:05:32 +1000

    updated data

--- data/json_converter.py
+++ data/json_converter.py
@@ -15,6 +15,26 @@ feature_set = {
     'features': []
 }
 
+feature_types = [
+  # 'Feature', default fallback
+  'Red Light Camera',
+  'Mobile Speed Camera',
+  'School Zone Speed Camera',
+  'School Zone',
+  'Safe-T-Cam',
+  'Speed Trap',
+  'Noise Camera',
+  'Fixed Speed Camera',
+  'Surveillance Camera',
+  'Tunnel Safety Camera',
+  'Average Speed Camera',
+  'Heavy Vehicle Safety Camera',
+  'Average Tunnel Speed Camera',
+  'Phone Detection Camera',
+  'Variable Speed Zone',
+  # 'Current Location', not included in feature set
+]
+
 # features
 #  - map from [x, y, name, description, accreditation]
 #  - map to { type, properties, geometry, id }
@@ -22,7 +42,7 @@ feature_set = {
 def row_to_feature(row):
     return {
         'id': 'feature-' + str(row['x']) + '-' + str(row['y']),
-        'type': 'Feature',
+        'type': get_feature_type(row),
         'properties': {
             'name': row['name'],
             'description': row['description'],
@@ -33,6 +53,16 @@ def row_to_feature(row):
             "coordinates": [row['x'], row['y']]
         }
     }
+
+
+def get_feature_type(row):
+    for t in feature_types:
+        if t in row['name']: return t
+    
+    if 'Red-Light' in row['name']: return 'Red Light Camera'
+    if 'Speed Cameras' in row['name']: return 'Fixed Speed Camera'
+
+    return 'Feature'
 
 mapped = df.fillna(value='').apply(row_to_feature, axis=1)
 

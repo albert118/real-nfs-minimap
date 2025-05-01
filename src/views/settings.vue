033aaa58b6edc254cd9b8216b873a2c790c0c91d
Albert Ferguson <albertferguson118@gmail.com>
2025-05-01 22:44:58 +1000

    simple card layout - need tailwind

--- src/views/settings.vue
+++ src/views/settings.vue
@@ -7,17 +7,41 @@ const { loading, clear: onClearCaches } = onClearCache();
 
 <template>
     <div class="stack-v">
-        <v-btn
-            outlined
-            @click.prevent="onClearCaches"
-            :loading="loading"
-            color="orange-darken-1"
-        >
-            <div class="pa-2">Clear Caches</div>
-        </v-btn>
+        <v-card>
+            <template v-slot:title>Developer Settings</template>
 
-        <v-btn outlined @click.prevent="onClearErrors" color="red-darken-1">
-            <div class="pa-2">Clear Errors</div></v-btn
-        >
+            <div class="stack-v pa-4">
+                <div
+                    class="stack-h"
+                    style="width: 100%; justify-content: space-between"
+                >
+                    <div class="text-caption">
+                        Clear all app and web worker caches
+                    </div>
+                    <v-btn
+                        outlined
+                        @click.prevent="onClearCaches"
+                        :loading="loading"
+                        color="orange-darken-1"
+                    >
+                        <div class="pa-2">Clear Caches</div>
+                    </v-btn>
+                </div>
+
+                <div
+                    class="stack-h"
+                    style="width: 100%; justify-content: space-between"
+                >
+                    <div class="text-caption">Clear all tracked errors</div>
+                    <v-btn
+                        outlined
+                        @click.prevent="onClearErrors"
+                        color="red-darken-1"
+                    >
+                        <div class="pa-2">Clear Errors</div>
+                    </v-btn>
+                </div>
+            </div>
+        </v-card>
     </div>
 </template>
